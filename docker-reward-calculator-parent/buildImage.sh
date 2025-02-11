a0de77b707344eab5a90eda4070b7765084f641b
suresh.dharisi <suresh.dharisi@gmail.com>
2025-02-11 17:10:51 -0500

    added few commands

--- docker-reward-calculator-parent/buildImage.sh
+++ docker-reward-calculator-parent/buildImage.sh
@@ -4,31 +4,31 @@ mvn clean install -DskipTests=true
 echo pushing reward-calculator-naming-server is started
 cd reward-calculator-naming-server
 docker build . -t reward-calculator-naming-server:1.0 
-docker tag  reward-calculator-naming-server:1.0 sureshdharisi123/reward-calculator-naming-server:latest
-docker push sureshdharisi123/reward-calculator-naming-server:latest
+docker tag  reward-calculator-naming-server:1.0 sureshdharisi123/reward-calculator-naming-server:1.0.0
+docker push sureshdharisi123/reward-calculator-naming-server:1.0.0
 cd ..
 echo pushing reward-calculator-naming-server is completed
 
 cd reward-points-store
 echo pushing reward-points-store is started
 docker build . -t reward-points-store:1.0 
-docker tag  reward-points-store:1.0 sureshdharisi123/reward-points-store:latest
-docker push sureshdharisi123/reward-points-store:latest
+docker tag  reward-points-store:1.0 sureshdharisi123/reward-points-store:1.0.0
+docker push sureshdharisi123/reward-points-store:1.0.0
 cd ..
 echo pushing reward-points-store is completed
 
 cd customer-transactions
 echo pushing customer-transactions is started
 docker build . -t customer-transactions:1.0 
-docker tag  customer-transactions:1.0 sureshdharisi123/customer-transactions:latest
-docker push sureshdharisi123/customer-transactions:latest
+docker tag  customer-transactions:1.0 sureshdharisi123/customer-transactions:1.0.0
+docker push sureshdharisi123/customer-transactions:1.0.0
 cd ..
 echo pushing customer-transactions is completed
 
 echo pushing rewards-calculator is started
 cd rewards-calculator
 docker build . -t rewards-calculator:1.0 
-docker tag  rewards-calculator:1.0 sureshdharisi123/rewards-calculator:latest
-docker push sureshdharisi123/rewards-calculator:latest
+docker tag  rewards-calculator:1.0 sureshdharisi123/rewards-calculator:1.0.0
+docker push sureshdharisi123/rewards-calculator:1.0.0
 cd ..
 echo pushing rewards-calculator is completed
